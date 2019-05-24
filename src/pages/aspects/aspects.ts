607a2bbc15bcf8a6640968cf693f8e056aa9a16b
angel-zepeda <angel.zepeda0508@hotmail.com>
2019-05-23 20:13:02 -0500

    modified emoji issue

--- src/pages/aspects/aspects.ts
+++ src/pages/aspects/aspects.ts
@@ -32,8 +32,8 @@ export class AspectsPage {
     // localStorage.setItem('mobImg', name);
     // const myModal = this.modal.create('ModalMercadeoPage');
     // myModal.present();
-    Swal.fire({      
-      imageUrl: ''+name
+    Swal.fire({
+      imageUrl: '' + name
       // animation: false
       //backdrop: `url('https://t3.ftcdn.net/jpg/01/30/82/72/240_F_130827267_qiwaSzorZ1Lixv5W2dPc1NXqVnxbpQiV.jpg')`
     })
@@ -50,15 +50,15 @@ export class AspectsPage {
     for (let a of aspects.aspects) {
       if (a.score === '') {
         count++;
-    //    missings.push(a.name + '\n');
+        //    missings.push(a.name + '\n');
       }
     }
     if (count > 0) {
       // swal("Oops!", `Falta evaluar: \n\n -${missings.join('-')}`, "error");
       Swal.fire({
-        html: '<h1 style="color:white; font-size:5rem;">Oops!'+
-              '</h1>' +
-              '<h2 style="color: white"> Faltan aspectos por evaluar. </h2>',
+        html: '<h1 style="color:white; font-size:5rem;">Oops!' +
+          '</h1>' +
+          '<h2 style="color: white"> Faltan aspectos por evaluar. </h2>',
         type: 'error',
         // background: '#B91919',
         background: 'rgba(0,0,0,0.7)',
@@ -72,9 +72,9 @@ export class AspectsPage {
         localStorage.setItem('report', JSON.stringify(report));
         // swal("Exito!", "Reporte guardado", "success");
         Swal.fire({
-          html: '<h1 style="color:white; font-size:4.55rem;">Exito!'+
-                '</h1>' +
-                '<h2 style="color: white">El reporte se ha llenado correctamente. </h2>',
+          html: '<h1 style="color:white; font-size:4.55rem;">Exito!' +
+            '</h1>' +
+            '<h2 style="color: white">El reporte se ha llenado correctamente. </h2>',
           type: 'success',
           // background: '#B91919',
           background: 'rgba(0,0,0,0.7)',
@@ -89,9 +89,9 @@ export class AspectsPage {
         localStorage.setItem('report', JSON.stringify(report));
         // swal("Exito!", "Reporte guardado", "success");
         Swal.fire({
-          html: '<h1 style="color:white; font-size:4.5rem;">Exito!'+
-                '</h1>' +
-                '<h2 style="color: white">El reporte se ha llenado correctamente. </h2>',
+          html: '<h1 style="color:white; font-size:4.5rem;">Exito!' +
+            '</h1>' +
+            '<h2 style="color: white">El reporte se ha llenado correctamente. </h2>',
           type: 'success',
           // background: '#B91919',
           background: 'rgba(0,0,0,0.7)',
@@ -103,11 +103,18 @@ export class AspectsPage {
     }
   }
 
-  active(emojiId) {
+  active(emojiId, name) {
     var emoji = document.getElementById(emojiId);
+
     if (emoji.id.includes('happy')) {
+      var emoji2 = document.getElementById('mad' + name);
+      var emoji3 = document.getElementById('angry' + name);
+      emoji2.style.background = "url('../../assets/imgs/3.png')";
+      emoji3.style.background = "url('../../assets/imgs/1.png')";
       emoji.style.background = "url('../../assets/imgs/55.png')";
       emoji.style.backgroundSize = "cover";
+      emoji2.style.backgroundSize = "cover";
+      emoji3.style.backgroundSize = "cover";
     }
     if (emoji.id.includes('confused')) {
       emoji.style.background = "url('../../assets/imgs/44.png')";
@@ -118,12 +125,24 @@ export class AspectsPage {
       emoji.style.backgroundSize = "cover";
     }
     if (emoji.id.includes('mad')) {
+      var emoji2 = document.getElementById('happy' + name);
+      var emoji3 = document.getElementById('angry' + name);
+      emoji2.style.background = "url('../../assets/imgs/5.png')";
+      emoji3.style.background = "url('../../assets/imgs/1.png')";
       emoji.style.background = "url('../../assets/imgs/22.png')";
       emoji.style.backgroundSize = "cover";
+      emoji2.style.backgroundSize = "cover";
+      emoji3.style.backgroundSize = "cover";
     }
     if (emoji.id.includes('angry')) {
+      var emoji2 = document.getElementById('mad' + name);
+      var emoji3 = document.getElementById('happy' + name);
+      emoji2.style.background = "url('../../assets/imgs/3.png')";
+      emoji3.style.background = "url('../../assets/imgs/5.png')";
       emoji.style.background = "url('../../assets/imgs/11.png')";
       emoji.style.backgroundSize = "cover";
+      emoji2.style.backgroundSize = "cover";
+      emoji3.style.backgroundSize = "cover";
     }
   }
 
@@ -147,3 +166,4 @@ export class AspectsPage {
     resHtml.innerHTML = "Resultado: " + res.toFixed(2);
   }
 }
+
